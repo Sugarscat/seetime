71c661066e080c18eb7d5eca8afa56fcc7bffab7
Sugarscat <tsugarcat@gmail.com>
2023-04-11 23:10:26 +0800

    update

---
+++ docs/.vitepress/nav/navbar.js
@@ -0,0 +1,6 @@
+export default [
+    {
+        text: '首页',
+        link: '/',
+    },
+]
---
+++ docs/.vitepress/nav/sidebar.js
@@ -0,0 +1,11 @@
+export default {
+    '/documents/': [
+        {
+            text: '目录',
+            collapsible: true,
+            items: [
+                {text: '开始', link: ''},
+            ]
+        },
+    ],
+}
